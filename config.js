51b590c4fbd17f32ae629484673d837a458d587d
Awan Berlian <berlianawan7@gmail.com>
2025-01-07 03:31:06 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  OPENAI_API_KEY: "sk-YOUR_OPENAI_API_KEY", // Ganti dengan API key OpenAI Anda
+  WEATHER_API_KEY: "8744f80a8a0541238fc175433250601", // Ganti dengan API key OpenWeatherMap Anda
+  DEFAULT_CITY: "Jakarta", // Kota default untuk cuaca
+};
